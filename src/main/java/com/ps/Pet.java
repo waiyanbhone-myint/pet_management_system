1cc97d00c785957ad02c40178024c055769a524a
Wai_Yan <waiyanbhonemyint2002@gmail.com>
2025-06-02 21:40:24 -0400

    -

---
+++ src/main/java/com/ps/Pet.java
@@ -0,0 +1,35 @@
+package com.ps;
+
+import javax.xml.namespace.QName;
+
+public class Pet {
+    private String name;
+    private int age;
+    private String type;
+
+    public Pet(String name, int age, String type) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
